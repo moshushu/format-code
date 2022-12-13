9962918aad35f0db1f3990d3737624611410ac1b
chen <chen@chen.local>
2022-12-13 10:43:14 +0800

    Flora代码构建器初步成型

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,111 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div>
+      <vue-editor />
+      <!-- primary 主要按钮 -->
+      <div>
+        <el-button type="primary" @click="addDomain">新增一行</el-button>
+      </div>
+      <br>
+      <div>
+        <el-form :model="formatDataForm" ref="formatDataForm" label-width="100px" :inline="true">
+
+          <div v-for="(domain, index) in formatDataForm.domains" :key="index">
+
+            <el-form-item :key="domain.key" label="实体中文名">
+              <el-input v-model="domain.entityNameCn" size="small" />
+            </el-form-item>
+
+            <el-form-item :key="domain.key" label="实体英文名">
+              <el-input v-model="domain.entityNameEn" size="small" />
+            </el-form-item>
+
+            <el-form-item :key="domain.key" label="字段名称">
+              <el-input v-model="domain.fieldName" size="small" />
+            </el-form-item>
+
+            <el-form-item :key="domain.key" label="字段类型">
+              <el-select v-model="domain.fieldType" size="small">
+                <el-option label="字符串" value="CharField"></el-option>
+                <el-option label="布尔" value="BooleanField"></el-option>
+                <el-option label="整数" value="IntegerField"></el-option>
+                <el-option label="单选" value="SelectionField"></el-option>
+                <el-option label="文本" value="TextField"></el-option>
+                <el-option label="日期" value="DateField"></el-option>
+                <el-option label="浮点数" value="FloatField"></el-option>
+                <el-option label="多选" value="MultiSelectionField"></el-option>
+              </el-select>
+            </el-form-item>
+
+            <el-form-item :key="domain.key" label="S和D">
+              <el-switch v-model="domain.isEnable" inline-prompt active-text="是" inactive-text="否" />
+            </el-form-item>
+
+            <el-form-item :key="domain.key" label="必填">
+              <el-switch v-model="domain.required" inline-prompt active-text="是" inactive-text="否" />
+            </el-form-item>
+
+            <el-form-item :key="domain.key" label="唯一">
+              <el-switch v-model="domain.unique" inline-prompt active-text="是" inactive-text="否" />
+            </el-form-item>
+
+            <el-form-item :key="domain.key" label="索引">
+              <el-switch v-model="domain.index" inline-prompt active-text="是" inactive-text="否" />
+            </el-form-item>
+
+          </div>
+
+        </el-form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import VueEditor from './components/vueEditor.vue'
 export default {
-  name: 'App',
+  name: 'dynamicForm',
+  // 引用vue-prism-editor 组件
   components: {
-    HelloWorld
-  }
+    'vue-editor': VueEditor
+  },
+  data() {
+    return {
+      formatDataForm: {
+        domains: [{
+          entityNameCn: '', // 实体中文名称
+          entityNameEn: '', // 实体英文名称
+          fieldName: '',    //字段名称
+          fieldType: '',    //字段类型
+          isEnable: true, //是否填写String和Description"
+          required: false, // 是否必填
+          unique: false, // 是否唯一
+          index: false, // 可否索引
+          // 单选多选默认携带选项框架
+          key: Date.now()
+        }],
+      },
+    }
+  },
+  methods: {
+    addDomain() {
+      this.formatDataForm.domains.push({
+        entityNameCn: '', // 实体中文名称
+        entityNameEn: '', // 实体英文名称
+        fieldName: '',    //字段名称
+        fieldType: '',    //字段类型
+        isEnable: true, //是否填写String和Description"
+        required: false, // 是否必填
+        unique: false, // 是否唯一
+        index: false, // 可否索引
+        // 单选多选默认携带选项框架
+        key: Date.now()
+      });
+    },
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/vueEditor.vue
@@ -0,0 +1,48 @@
+<template>
+  <!--  code：为需要高亮显示的代码内容，highlighter：定义在methods中的一个方法，用于把code高亮显示，lineNumbers：是否可编辑标识 -->
+  <prism-editor class="my-editor" v-model="code" :highlight="highlighter" line-numbers></prism-editor>
+</template>
+
+<script>
+// import Prism Editor
+import { PrismEditor } from 'vue-prism-editor';
+import 'vue-prism-editor/dist/prismeditor.min.css'; // import the styles somewhere
+
+// import highlighting library (you can use any library you want just return html string)
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/themes/prism-tomorrow.css'; // import syntax highlighting styles
+
+export default {
+  components: {
+    PrismEditor,
+  },
+  data: () => ({ code: '什么都没有' }),
+  // 高亮方法
+  methods: {
+    highlighter(code) {
+      return highlight(code, languages.js); // languages.<insert language> to return html with markup
+    },
+  },
+};
+</script>
+
+<style>
+/* required class */
+.my-editor {
+  
+  background: #2d2d2d;
+  color: #ccc;
+
+  font-family: Fira code, Fira Mono, Consolas, Menlo, Courier, monospace;
+  font-size: 14px;
+  line-height: 1.5;
+  padding: 5px;
+}
+
+/* optional class for removing the outline */
+.prism-editor__textarea:focus {
+  outline: none;
+}
+</style>
